458d8d8a88dfd6029c5d664b283278debc3afa8a
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-18 18:42:22 -0300

    chore: Add Singleton pattern in repository

--- src/roles/useCases/listRoles/index.ts
+++ src/roles/useCases/listRoles/index.ts
@@ -2,6 +2,6 @@ import { RolesRepository } from "@roles/repositories/RolesRepository";
 import { ListRolesUseCases } from "./ListRolesUseCase";
 import { ListRolesController } from "./ListRolesController";
 
-const rolesRepository = new RolesRepository()
+const rolesRepository = RolesRepository.getInstance()
 const listRolesUseCase = new ListRolesUseCases(rolesRepository)
 export const listRolesController = new ListRolesController(listRolesUseCase)
