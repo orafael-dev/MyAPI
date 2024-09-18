33fdd511db1e6eac39c98ff93c72d433bc46325f
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-18 11:05:05 -0300

    chore: Add error middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,9 @@ import { Router } from "express";
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new Error('Access denied')
   return response.json({ message: 'Olá Dev!' })
 })
 
 export {routes}
+
