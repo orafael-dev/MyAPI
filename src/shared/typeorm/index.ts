97341c1934e6178803ceda0030c36925eef83125
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-19 11:01:15 -0300

    refactor: Role entity using decorators

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm";
 import { CreateRolesTable1726751399204 } from "./migrations/1726751399204-CreateRolesTable";
+import { Role } from "@roles/entitites/roles";
 
 export const dataSource = new DataSource({
     type: "sqlite",
     database: "./db.sqlite",
-    entities: [],
+    entities: [Role],
     migrations: [CreateRolesTable1726751399204],
 
 })
