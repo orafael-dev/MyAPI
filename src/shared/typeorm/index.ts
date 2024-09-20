fe5a96fbdb90030b51e2d2307661b26246b9c131
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-20 15:10:41 -0300

    feat: create migrations users table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,11 +1,12 @@
 import { DataSource } from "typeorm";
 import { Role } from "@roles/entitites/roles";
 import { CreateRolesTable1726770440518 } from "./migrations/1726770440518-CreateRolesTable";
+import { CreateUsersTable1726854494728 } from "./migrations/1726854494728-CreateUsersTable";
 
 export const dataSource = new DataSource({
     type: "sqlite",
     database: "./db.sqlite",
     entities: [Role],
-    migrations: [CreateRolesTable1726770440518],
+    migrations: [CreateRolesTable1726770440518, CreateUsersTable1726854494728],
 
 })
