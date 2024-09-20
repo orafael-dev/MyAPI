a1bc75fd93b5a08ec5418e5bfc510951dd948667
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-20 18:30:52 -0300

    chore: Add ListUsers usecase

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from '@users/repositories/IUsersRepository'
 import { UsersRepository } from '@users/repositories/UserRepository'
 import { CreateUserController } from '@users/useCases/createUser/CreateUserController'
+import { ListUsersController } from '@users/useCases/listUsers/ListUsersController'
 import { container } from 'tsyringe'
 
 container.registerSingleton<IUsersRepository>(
@@ -9,3 +10,5 @@ container.registerSingleton<IUsersRepository>(
 )
 
 container.registerSingleton('CreateUserController', CreateUserController)
+
+container.registerSingleton('ListUsersController', ListUsersController)
