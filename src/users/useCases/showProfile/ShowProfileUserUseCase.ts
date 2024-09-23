9ed86529f4be1691f565f4793395904ff573dfa7
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-23 16:13:10 -0300

    feat: create ShowProfile usecasse and controller

---
+++ src/users/useCases/showProfile/ShowProfileUserUseCase.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from 'tsyringe'
+import { AppError } from '@shared/errors/AppError'
+import { IUsersRepository } from '@users/repositories/IUsersRepository'
+import { User } from '@users/entities/User'
+
+type ShowProfileParams = {
+  userId: string
+}
+
+@injectable()
+export class ShowProfileUseCase {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+      userId,
+  }: ShowProfileParams): Promise<User> {
+    const user = await this.usersRepository.findById(userId)
+    if (!user) {
+      throw new AppError('User not found.')
+    }
+
+    return user
+  }
+}
