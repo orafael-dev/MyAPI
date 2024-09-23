e2190573e8d36bc5f2339e4221e660c2d7711bb3
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-23 12:11:00 -0300

    feat: add user avatar_url properties

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from 'express'
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
+import path from 'node:path'
+import uploadConfig from '@config/upload'
 import { errors } from 'celebrate'
 import { routes } from './routes'
 import { AppError } from '@shared/errors/AppError'
@@ -12,6 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 app.use(errors())
--- src/users/entities/User.ts
+++ src/users/entities/User.ts
@@ -1,6 +1,12 @@
 import { Role } from '@roles/entitites/roles'
-import { Exclude } from 'class-transformer'
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn } from 'typeorm'
+import { Exclude, Expose } from 'class-transformer'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
 @Entity('users')
@@ -25,15 +31,23 @@ export class User {
   avatar?: string
 
   @ManyToOne(() => Role, {
-    cascade: true
+    cascade: true,
   })
   role: Role
 
   @CreateDateColumn()
   created_at: Date
 
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    if (!this.avatar) {
+      return null
+    }
+    return `${process.env.AVATAR_URL}/${encodeURIComponent(this.avatar)}`
+  }
+
   constructor() {
-    if(!this.id) {
+    if (!this.id) {
       this.id = uuidv4()
     }
   }
